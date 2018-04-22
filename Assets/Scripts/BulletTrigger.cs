b19c5c0b8711e0f65c665a1918eb9217b736f2e8
Rob Miller <cowboysfn007@gmail.com>
2018-04-22 10:30:07 -0700

    finished creating gun and bullet, destroys bullet on collision, kills player on hit

---
+++ Assets/Scripts/BulletTrigger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletTrigger : MonoBehaviour {
+
+    void OnCollisionEnter(Collision collision) {
+        if (collision.gameObject.tag == "Player") {
+            PlayerController controller = collision.gameObject.GetComponent("PlayerController") as PlayerController;
+            controller.KillPlayer();
+            Destroy(gameObject);
+        } else {
+            Destroy(gameObject);
+        }
+    }
+
+}
